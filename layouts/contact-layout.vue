db71546ecc86244d20d432bd63caf9695bbcb45b
Ivan Doric <ivan.doric@burza.hr>
2019-04-13 08:56:06 +0200

    Static Sites With Nuxt - 03 - Views and SEO

---
+++ layouts/contact-layout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Contact Layout</h2>
+
+        <nuxt />
+
+        <footer>
+            This is contact layout footer
+        </footer>
+    </div>
+</template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,3 +3,18 @@
         Contact page
     </div>
 </template>
+
+<script>
+    export default {
+        layout: 'contact-layout',
+        head() {
+            return {
+                title: 'Contact page',
+                meta: [
+                    {hid: 'description', name: 'description', content: 'My description'},
+                    {hid: 'ogtitle', property: 'og:title', content: 'Contact page'}
+                ]
+            }
+        }
+    }
+</script>
